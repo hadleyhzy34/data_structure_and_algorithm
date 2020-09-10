7dfb7e604d9e14c444f86601b371370ec078f793
Hadley <Hadley_hzy@hotmail.com>
2020-09-10 14:51:22 +0200

    update

--- graph/Graph.cpp
+++ graph/Graph.cpp
@@ -35,8 +35,34 @@ class graph
     {
         adj[v].push_back(w); // Add w to v’s list.
     }
+    
+    void DFSUtil(int v, bool visited[]);
+    void DFS(); //dfs approach to print graph nodes
+    void BFS(); //bfs approach to print graph nodes
 }
 
+void Graph::DFSUtil(int v , bool visited[]) {
+    visited[v] = true;
+    cout << v << " ";
+    
+    for (auto it = adj[v].begin(); it != adj[v].end(); ++it) {
+        if (!visited[*it]) {
+            DFSUtil(*it, visited);
+        }
+    }
+}
+
+void Graph::DFS(){
+    
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for(int i = 0; i < V; i++)
+        visited[i] = false;
+    //run DFS from all unvisited nodes including disconnected graph
+    for(int i = 0; i < V; i++)
+        if(visited[i] == false)
+            DFSUtil(i, visited);
+}
 //using vector for graph node representation
 class Node{
 public:
--- graph/README.md
+++ graph/README.md
@@ -41,6 +41,31 @@ public:
 
 
 ## Depth First Search for a Graph
+```c++
+void Graph::DFSUtil(int v , bool visited[]) {
+    visited[v] = true;
+    cout << v << " ";
+    
+    for (auto it = adj[v].begin(); it != adj[v].end(); ++it) {
+        if (!visited[*it]) {
+            DFSUtil(*it, visited);
+        }
+    }
+}
+
+void Graph::DFS(){
+    
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for(int i = 0; i < V; i++)
+        visited[i] = false;
+    //run DFS from all unvisited nodes including disconnected graph
+    for(int i = 0; i < V; i++)
+        if(visited[i] == false)
+            DFSUtil(i, visited);
+}
+```
+
 
 ## Breadth First Search for a Graph
 
