53156e508052115f1259d4ef354a7161e47cb987
Hadley <Hadley_hzy@hotmail.com>
2020-10-05 12:50:44 +0200

    update

--- linked_list/leetcode_linked_list/82_remove_duplicates_from_sorted_list_2.cpp
+++ linked_list/leetcode_linked_list/82_remove_duplicates_from_sorted_list_2.cpp
@@ -22,26 +22,8 @@ using namespace std;
 
 class Solution {
 public:
-    //delete duplicated numbers
-//    ListNode* deleteDuplicates(ListNode* head) {
-//        ListNode* cur=head;
-//        ListNode* pre=head;
-//        while(cur){
-//            if(cur->val==pre->val){
-//                cur=cur->next;
-//            }else{
-//                pre->next=cur;
-//                pre=cur;
-//                cur=cur->next;
-//            }
-//        }
-//        pre->next=nullptr;
-//        return head;
-//    }
-    
     //delete numbers that are duplicated
     ListNode* deleteDuplicates(ListNode* head) {
-        ListNode* res=nullptr;
         if(!head)return nullptr;
         
         //previous node of previous node
@@ -49,41 +31,117 @@ public:
         //previous node of current node
         ListNode* pre_cur=nullptr;
         //current node
-        ListNode* cur=nullptr;
+        ListNode* cur=head;
         
-        
-        if(head->next){
-            if(head->val!=head->next->val){
-                res=head;
-            }
-            pre_pre=head;
-            pre_cur=head->next;
-        }else{
-            return head;
-        }
-        
-        if(head->next->next){
-            cur=head->next->next;
-        }else{
-            return res;
-        }
+        //final first node for output
+        ListNode* res=new ListNode;
+        ListNode dummy(0);
+        dummy.next=res;
         
         while(cur){
-            if(pre_cur->val!=pre_pre->val&&pre_cur->val!=cur->val){
-                if(res){
-                    head->next=pre_cur;
-                    head=head->next;
-                }else{
-                    res=pre_cur;
+            if(pre_cur){
+                if(pre_pre){     //when it loops to at least more than three nodes
+                    if(pre_cur->val!=pre_pre->val&&pre_cur->val!=cur->val){
+                        res->next=pre_cur;
+                        res=res->next;
+                    }
+                }else{           //pre_per is not assigned yet
+                    if(pre_cur->val!=cur->val){
+                        res->next=pre_cur;
+                        res=res->next;
+                    }
+                }                 //move all three nodes forward
+                pre_pre=pre_cur;
+                pre_cur=cur;
+                cur=cur->next;
+            }else{
+                pre_cur=cur;
+                if(!cur->next){  //if there's only one node
+                    res->next=cur;
+                    res=res->next;
                 }
+                cur=cur->next;
             }
-            pre_pre=pre_cur;
-            pre_cur=cur;
-            cur=cur->next;
         }
-        if(pre_cur->val!=pre_pre->val){
-            head->next=pre_cur;
+
+        if(pre_cur&&pre_pre){       //check last node where cur node is nullptr
+            if(pre_cur->val!=pre_pre->val){
+                res->next=pre_cur;
+                res=res->next;
+            }
         }
-        return res;
+        res->next=nullptr;
+        return dummy.next->next;
     }
 };
+
+class Solution2 {
+public:
+    //delete numbers that are duplicated
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head)return nullptr;
+        ListNode * dummy = new ListNode;
+        dummy->next=head;
+        ListNode *pre=dummy, *cur=head, *next=head->next;
+        
+        bool flag=false;
+        while(next){
+            if(cur->val!=next->val){
+                //if cur->val==pre->val
+                if(flag){
+                    pre->next=next;
+                    cur=next;
+                    next=next->next;
+                }else{  // not duplicated
+                    pre=pre->next;
+                    cur=cur->next;
+                    next=next->next;
+                }
+                flag=false;
+            }else{
+                flag=true;
+                next=next->next;
+            }
+        }
+        
+        //
+        if(flag)pre->next=nullptr;
+        return dummy->next;
+    }
+};
+
+class Solution3 {
+public:
+    //delete numbers that are duplicated
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head)return nullptr;
+        ListNode dummy(0);
+        dummy.next=head;
+        ListNode *candidate=&dummy, *cur=head, *next=head->next;
+        
+        //flag to check if cur contains duplicated value or not
+        bool flag=false;
+        while(next){
+            if(next->val!=cur->val){
+                //if curent node is not duplicated and next node is not same as cur node, then cur node is one for final output
+                if(!flag){
+                    candidate->next=cur;
+                    candidate=cur;
+                    cur=next;
+                    next=next->next;
+                }else{//if current node is duplicated one
+                    cur=next;
+                    next=next->next;
+                }
+                flag=false;
+            }else{
+                flag=true;
+                //keep cur node, move next node to next node, in the future to compare with this duplicated cur node
+                next=next->next;
+            }
+        }
+        
+        return dummy.next;
+    }
+};
+
---
+++ tree/leetcode_tree/think_cell.cpp
@@ -0,0 +1,47 @@
+//
+//  think_cell.cpp
+//  leetcode_tree
+//
+//  Created by Hadley on 04.10.20.
+//  Copyright © 2020 Hadley. All rights reserved.
+//
+
+#include <map>
+template<typename K, typename V>
+class interval_map {
+    friend void IntervalMapTest();
+    V m_valBegin;
+    std::map<K,V> m_map;
+public:
+    // constructor associates whole range of K with val
+    interval_map(V const& val)
+    : m_valBegin(val)
+    {}
+
+    // Assign value val to interval [keyBegin, keyEnd).
+    // Overwrite previous values in this interval.
+    // Conforming to the C++ Standard Library conventions, the interval
+    // includes keyBegin, but excludes keyEnd.
+    // If !( keyBegin < keyEnd ), this designates an empty interval,
+    // and assign must do nothing.
+    void assign( K const& keyBegin, K const& keyEnd, V const& val ) {
+// INSERT YOUR SOLUTION HERE
+        
+    }
+
+    // look-up of the value associated with key
+    V const& operator[]( K const& key ) const {
+        auto it=m_map.upper_bound(key);
+        if(it==m_map.begin()) {
+            return m_valBegin;
+        } else {
+            return (--it)->second;
+        }
+    }
+};
+
+// Many solutions we receive are incorrect. Consider using a randomized test
+// to discover the cases that your implementation does not handle correctly.
+// We recommend to implement a test function that tests the functionality of
+// the interval_map, for example using a map of int intervals to char.
+
