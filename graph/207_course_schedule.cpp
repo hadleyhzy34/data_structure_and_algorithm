4ed4e3df6e8d6fd99645926a59138f78da548323
Hadley <Hadley_hzy@hotmail.com>
2020-10-20 08:29:34 +0200

    update

--- graph/207_course_schedule.cpp
+++ graph/207_course_schedule.cpp
@@ -1,12 +1,43 @@
+
+//topological sort
 class Solution {
 public:
-	bool detectCycle(vector<vector<int>>&edges, ){
-		
-	}
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> edges(n,vector<int>(n));
+                vector<vector<int>> edges(numCourses,vector<int>());
+        queue<int> nodes;
+        vector<int> indegrees(numCourses,0);
+        vector<int> res;
+        //x[0] courses to be taken, x[1] courses taken before x[0]
         for(auto x:prerequisites){
-        	edges[x[0]].push_back(x[1]);
+        	edges[x[1]].push_back(x[0]);
+        	++indegrees[x[0]];
         }
+
+        for(int i=0;i<indegrees.size();i++){
+        	if(indegrees[i]==0){
+        		nodes.push(i);
+        	}
+        }
+
+        int visit_node=0;
+        while(!nodes.empty()){
+        	int index=nodes.front();
+        	nodes.pop();
+        	visit_node++;
+
+        	res.push_back(index);
+        	for(auto i:edges[index]){
+        		indegrees[i]--;
+        		if(indegrees[i]==0){
+        			nodes.push(i);
+        		}
+        	}
+        }
+        // if(visit_node==numCourses){
+        //     return res;
+        // }else{
+        //     return {};
+        // }
+        return visit_node==numCourses?res:vector<int>();
     }
 };
