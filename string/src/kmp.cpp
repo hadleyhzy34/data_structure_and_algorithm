f601ecc8296f7f0f9545c5a1ac406b1d3e4c82ba
hadleyhzy34 <hadley_hzy@hotmail.com>
2023-01-31 00:21:16 +0800

    update

--- string/src/kmp.cpp
+++ string/src/kmp.cpp
@@ -24,32 +24,45 @@ void computeLPS(std::string pattern, int* lps)
                 i++;
             }else{
                 len = lps[len-1];
+            }
         }
     }
 }
 
 int kmp(std::string txt, std::string pat)
 {
-    int m = txt.size();
-    int n = pat.size();
+    int m = txt.size(); //length of text
+    int n = pat.size(); //length of pattern
 
     int lps[n];
-    computeLPS(pat,lps);
+    computeLPS(pat,lps); //initialize longest prefix and suffix
 
     int i=0; //txt
     int j=0; //pat
-    while(i<m)
+    
+    while(j<n)
     {
         if(txt[i] == pat[j])
         {
             j++;
             i++;
         }
-        if(i == m){
+        if(j == n){
             return i - j;
-        }else if(){
-
+        }else{
+            if(txt[i] != pat[j]){
+                if(i == 0){
+                    i++;
+                }else{
+                    computeLPS(pat,lps);
+                }
+            }
         }
     }
     return -1; //return -1 if tgt not matched src
+}
+
+int main(){
+
+    return 0;
 }
